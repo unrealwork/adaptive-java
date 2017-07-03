6ef1cffc84ec93e16c5d48cf04406cef6f27e42a
unrealwork <unrealwork@gmail.com>
2017-07-03 13:01:45 +0300

    +5 tasks

---
+++ src/main/java/com/unrealwork/adaptive/java/problems/ReverseNumber.java
@@ -0,0 +1,13 @@
+package com.unrealwork.adaptive.java.problems;
+
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    StringBuilder sb = new StringBuilder(in.next());
+    System.out.println(sb.reverse().toString());
+  }
+}
