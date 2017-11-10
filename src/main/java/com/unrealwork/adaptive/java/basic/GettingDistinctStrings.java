ec6fc7f4fd31deb3beed61692544f241e5196cdc
unrealwork <unrealwork@gmail.com>
2017-11-11 02:49:24 +0300

    +5

---
+++ src/main/java/com/unrealwork/adaptive/java/basic/GettingDistinctStrings.java
@@ -0,0 +1,13 @@
+package com.unrealwork.adaptive.java.basic;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class GettingDistinctStrings {
+
+  public static void main(String[] args) {
+    Function<List<String>, List<String>> lambda = x -> x.stream().distinct()
+        .collect(Collectors.toList());
+  }
+}
---
+++ src/main/java/com/unrealwork/adaptive/java/basic/Matrix2.java
@@ -0,0 +1,28 @@
+package com.unrealwork.adaptive.java.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Matrix2 {
+
+  private static final char SPACE = ' ';
+  public static final String LINE_SEPARATOR = System.lineSeparator();
+
+  public static void main(String[] args) {
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));) {
+      final int gridSize = Integer.parseInt(reader.readLine());
+      final StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < gridSize; i++) {
+        for (int j = 0; j < gridSize; j++) {
+          sb.append(Math.abs(i - j));
+          sb.append(SPACE);
+        }
+        sb.append(LINE_SEPARATOR);
+      }
+      System.out.println(sb.toString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
